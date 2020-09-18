0044836885bddef1a9b80fa0996f3c525a8d99c3
Max Base <MaxBaseCode@Gmail.Com>
2020-09-19 03:06:57 +0430

    Create examples.php

---
+++ examples.php
@@ -0,0 +1,16 @@
+<?php
+<?php
+/*
+ * @Name: WebPostPHP
+ * @Author: Max Base
+ * @Date: 2020-09-19
+ * @Repository: https://github.com/BaseMax/WebPostPHP
+ */
+require "src/WebPost.php";
+
+$WebPost=new WebPostPHP();
+$WebPost->add("index.php", "name=adam&city=Berlin&Age=30", "Click here");
+$WebPost->add("index.php?name=adam&city=Berlin&Age=30", "Click");
+// print_r($WebPost->scripts);
+// $WebPost->scripts();
+
